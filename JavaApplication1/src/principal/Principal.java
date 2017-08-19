bb2674681ca3651b231155468d16eb9d8c491e3b
Angel <Angel@192.168.1.17>
2017-08-19 18:15:36 -0500

    Final

---
+++ JavaApplication1/src/principal/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new Ventana().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
---
+++ JavaApplication1/src/principal/Sumador.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Sumador {
+    
+    int operando1;
+    int operando2;
+    
+    int sumar(){
+        return(operando1+operando2);
+    }
+    
+}
